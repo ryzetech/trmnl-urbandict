bdd7699efa29b45a0b350ba82fc262c4c5b0a660
ryzetech <git@finnley.dev>
2025-06-06 20:17:39 +0200

    draw the rest of the fucking owl

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const urbanMainPageResponse = await fetch('https://www.urbandictionary.com');
+
+		if (!urbanMainPageResponse.ok) {
+			return new Response('Failed to fetch Urban Dictionary main page', { status: urbanMainPageResponse.status });
+		}
+
+		const urbanMainPageText = await urbanMainPageResponse.text();
+
+		// regex the first define link from that mess of a page
+		const wordExtractor = /href="\/define.php\?term=([^"]+)"/;
+		const wordPath = urbanMainPageText.match(wordExtractor);
+
+		if (!wordPath || wordPath.length < 2) {
+			return new Response('No word found on Urban Dictionary main page', { status: 404 });
+		}
+
+		// undocumented api goes brrr
+		const urbanWordResponse = await fetch("https://api.urbandictionary.com/v0/define?term=" + encodeURIComponent(wordPath[1]));
+
+		if (!urbanWordResponse.ok) {
+			return new Response('Failed to fetch Urban Dictionary word', { status: urbanWordResponse.status });
+		}
+		const urbanWordData = await urbanWordResponse.json();
+
+		if (!urbanWordData || urbanWordData.length === 0) {
+			return new Response('No definition found for the word', { status: 404 });
+		}
+
+		// we only want the first definition and rewrite it a bit
+		let urbanWordDataJson = urbanWordData.list[0];
+
+		// clean up that bish
+		urbanWordDataJson.definition = urbanWordDataJson.definition.replace(/\[|\]/g, '');
+		urbanWordDataJson.example = urbanWordDataJson.example.replace(/\[|\]/g, '');
+
+		return new Response(JSON.stringify(urbanWordDataJson), {
+			status: 200,
+			headers: {'Content-Type': 'application/json'},
+		});
 	},
 };
